7a974329933e039a5c6759ce29f63bdee8b97323
geetarOS <connerpchrist@gmail.com>
2026-06-04 15:39:15 -0400

    new theme

--- system/install.sh
+++ system/install.sh
@@ -11,8 +11,10 @@ sudo cp "$REPO/etc/sddm.conf" /etc/sddm.conf
 
 # silent theme customizations (theme itself from sddm-silent-theme AUR pkg)
 sudo cp "$REPO/usr/share/sddm/themes/silent/configs/custom.conf" "$THEME/configs/"
+sudo cp "$REPO/usr/share/sddm/themes/silent/configs/custom2.conf" "$THEME/configs/"
 sudo cp "$REPO/usr/share/sddm/themes/silent/metadata.desktop" "$THEME/"
+sudo cp "$REPO/usr/share/sddm/themes/silent/backgrounds/marble.jpg" "$THEME/backgrounds/"
 sudo cp "$REPO/usr/share/sddm/themes/silent/backgrounds/tokyonight.png" "$THEME/backgrounds/"
-sudo install -Dm644 "$REPO/assets/face.icon" "/usr/share/sddm/faces/${TARGET_USER}.face.icon"
+sudo install -Dm644 "$REPO/../assets/face.icon" "/usr/share/sddm/faces/${TARGET_USER}.face.icon"
 
 echo "SDDM config + silent customizations deployed."
