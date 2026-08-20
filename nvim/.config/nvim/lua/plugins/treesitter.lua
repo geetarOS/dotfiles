4205e088e497240dd6a0f7cd6baf6a40a8062809
geetarOS <connerpchrist@gmail.com>
2026-08-20 12:17:32 -0400

    more

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,30 @@
--- treesitter.lua
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = { "lua", "vim", "vimdoc", "bash" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+    config = function()
+      local ensure_installed = { "lua", "vim", "vimdoc", "bash", "markdown", "markdown_inline" }
+      local installed = require("nvim-treesitter.config").get_installed()
+      local missing = vim.iter(ensure_installed)
+        :filter(function(p) return not vim.tbl_contains(installed, p) end)
+        :totable()
+      if #missing > 0 then
+        require("nvim-treesitter").install(missing)
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local lang = vim.treesitter.language.get_lang(args.match)
+          if not lang or not vim.tbl_contains(
+            require("nvim-treesitter.config").get_installed(), lang) then
+            return
+          end
+          pcall(vim.treesitter.start)
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end,
   },
 }
